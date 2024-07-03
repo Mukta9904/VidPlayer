e8372ba4338abdb0b484afc509a4f34c59a9bc07
Mukta9904 <muktadas9904@gmail.com>
2024-07-03 16:19:58 +0530

    connect to mongo atlas

---
+++ src/db/index.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+async function connectDB() {
+    try {
+       const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+       console.log(`MongoDB connected successfully!! DB Host: ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.log("Mongodb connection fail", error);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
---
+++ src/index.js
@@ -0,0 +1,27 @@
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants";
+// import express from "express"
+
+// const app = express();
+
+// ;(async ()=>{
+//    try {
+//     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//     app.on("error", (error)=>{
+//         console.error(error);
+//         throw error;
+//     })
+//    } catch (error) {
+//     console.error(error);
+//      throw error;
+//    }
+// })()
+// require(dotenv).config({path: "./env"})
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: "./env"
+})
+
+connectDB()
